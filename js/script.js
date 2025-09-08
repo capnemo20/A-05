7dc3d39022646dcd39b94020a8e2030248e9e2bd
capnemo20 <z.sujon20@outlook.com>
2025-09-08 04:05:36 +0100

    reducing coins in order to make call done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
+// <<<<<<<<<<<<<------------------------->>>>>>>>>>>>>>
 // heart icon adding function
-const getHeartCountId = document.getElementById("heart-count")
+const getHeartCountId = document.getElementById("heart-count");
 const heartIcons = document.getElementsByClassName("heart-icon");
 let heartCount = 0;
 
-for (const icon of heartIcons){
-    icon.addEventListener("click",()=>{
-        heartCount++;
-        getHeartCountId.innerText = heartCount
-    })
+for (const icon of heartIcons) {
+  icon.addEventListener("click", () => {
+    heartCount++;
+    getHeartCountId.innerText = heartCount;
+  });
+}
+
+// <<<<<<<<<<<<<------------------------->>>>>>>>>>>>>>
+
+// Call functionality
+const coinsForCallId = document.getElementById("coins-for-call");
+const callButtons = document.getElementsByClassName("call");
+let coins = 100;
+
+for (const btn of callButtons) {
+  btn.addEventListener("click", () => {
+    if (coins < 20) {
+      alert("Sorry, you do not have sufficient coins to make a call");
+    } else {
+      coins = coins - 20;
+      coinsForCallId.innerText = coins;
+    }
+  });
 }
